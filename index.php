562d647cfdf8309c6ac71d5e2d0c9fe7edb875f1
ishimweghislain <ishimweghislain82@gmail.com>
2024-09-02 23:07:02 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1IbXyVY4AnDQlJlnGp1JcuT7jzwjju9SvrLVRyOoPp2hbWrGmJQ1iXwJSp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-TcSIqQb027qvyjSMfHjOMaLkfuwVxZzUPnCJA7lzmcCWNIP3zVgnCgpD7Xa" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <nav>
+        <label class="logo" style="color: #267860;">W-school</label>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Admission</a></li>
+            <li><a href="" class="btn btn-success ">Login</a></li>
+        </ul>
+    </nav>
+    <div class="section1">
+
+        <img src="img/school.jpeg" style="width: 100%; height: 600px; filter: brightness(0.6);" alt="">
+        <div class="content-overlay" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); color: white;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <!-- Decorative lines and text -->
+                        <hr style="border-top: 2px solid rgb(0, 240, 0); width: 50%; margin: 0 auto;">
+                        <h2 class="text-uppercase" style="font-size: 3.5em; margin-top: 80px; color: rgb(0, 240, 0);">Welcome to Our School</h2>
+                        <p style="font-size: 1.5em;">Our school is known for its excellent education system, modern facilities, and commitment to student success. We foster a community of learning and innovation where students are encouraged to thrive.</p>
+                        <hr style="border-top: 2px solid rgb(0, 240, 0); width: 50%; margin: 0 auto;">
+                        <a href="#" class="btn btn-success" style="margin-top: 60px; font-size: 20px;">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container" style="padding-top: 70px;">
+        <div class="row">
+            <div class="col-md-4">
+            <img src="img/building.jpeg" style="width: 100%; height: 250px;" alt="">
+            </div>
+           <div class="col-md-8"></div>
+            <h1 style="color: #267860;">Welcome to Our School .</h1>
+            <p>
+                MEMS has been committed to global learning long before it became an indispensable feature of contemporary education. Established in 2024, we proudly stand as the 1st English medium school in Bangladesh to adopt both Pearson Edexcel and Cambridge curriculum (in O and A levels), drawing together students in a vibrant, academically challenging, and encouraging environment where manifold viewpoints are prized and celebrated. MEMS has been committed to global learning long before it became an indispensable feature of contemporary education. Established in 1997, we proudly stand as the 1st English medium school in Bangladesh to adopt both Pearson Edexcel and Cambridge curriculum (in O and A levels), drawing together students in a vibrant, academically challenging, and encouraging environment where manifold viewpoints are prized and celebrated.
+            </p>
+            
+        </div>
+    </div>
+    <center>
+        <h1 style="color: #267860;" >Our Teachers</h1>
+    </center>
+    
+    <div class="container">
+        <div class="row">
+       <div class="col-md-4">
+         <img src="img/teacher.jpeg" style="width: 90%; height: 200px;" alt="">
+         <p>We have world wide teachers who have an astonishing expreince of teaching which will be a good impact to your child.</p>
+       </div>
+       <div class="col-md-4">
+        <img src="img/teacher2.jpeg" style="width: 90%; height: 200px;" alt="">
+        <p>We look forward to giving good education and support your children for them to cross countries with their epic ideas.</p>
+       </div>
+       <div class="col-md-4">
+       <img src="img/teacher3.jpeg" style="width: 90%; height: 200px;" alt="">
+       <p>We also don't give basic knowledge and skills to your child but also we give chances to study abroad and get interanational competitions.</p>
+       </div>
+        </div>
+    </div>
+    <center>
+        <h1 style="color: #267860;">Our Courses</h1>
+    </center>
+    
+    <div class="container">
+        <div class="row">
+       <div class="col-md-4">
+         <img src="img/math.jpeg" style="width: 90%; height: 200px;" alt="">
+         <p>Our school offers an outstanding mathematics program taught by highly experienced and passionate teachers. We provide an engaging and comprehensive curriculum that builds a strong foundation in math skills, encouraging critical thinking and problem-solving abilities in students.</p>
+       </div>
+       <div class="col-md-4">
+        <img src="img/science.jpeg" style="width: 90%; height: 200px;" alt="Science Image">
+        <p>Our science program is designed to inspire curiosity and foster a love for discovery. With hands-on experiments and state-of-the-art labs, we encourage students to explore the wonders of science and develop innovative ideas that can change the world.</p>
+       </div>
+       <div class="col-md-4">
+        <img src="img/lang.jpeg" style="width: 90%; height: 200px;" alt="Language Studies Image">
+        <p>Our language programs go beyond teaching basic communication skills. We offer immersive experiences that open doors to studying abroad, participating in international competitions, and fostering cultural exchange. We empower students to become global citizens, fluent in languages and understanding diverse cultures.</p>
+        
+       </div>
+        </div>
+    </div>
+
+    <center>
+        <h1 style="color: #267860;">Admission Form !</h1>
+    </center>
+    <div align="center" class="admission form">
+        
+        <form action="">
+            <div class="form" style="margin-right: -80px;">
+            <div class="row mb-3" style="margin-bottom: 20px;">
+                <div class="col-md-6">
+                    <label class="form-label" for="name">Full Names :</label>
+                </div>
+                <div class="col-md-6">
+                    <input class="form-control" style="width: 400px; margin-left: -820px;" type="text" name="name" id="name">
+                </div>
+            </div>
+            <div class="row mb-3" style="margin-bottom: 20px;">
+                <div class="col-md-6">
+                    <label class="form-label" for="email">Email :</label>
+                </div>
+                <div class="col-md-6">
+                    <input class="form-control" style="width: 400px; margin-left: -820px;" type="text" name="email" id="email">
+                </div>
+            </div>
+            <div class="row mb-3" style="margin-bottom: 20px;">
+                <div class="col-md-6">
+                    <label class="form-label" for="phone">Phone :</label>
+                </div>
+                <div class="col-md-6">
+                    <input class="form-control" style="width: 400px; margin-left: -820px;" type="text" name="phone" id="phone">
+                </div>
+            </div>
+            <div class="row mb-3" style="margin-bottom: 20px;">
+                <div class="col-md-6">
+                    <label class="form-label" for="message">Message :</label>
+                </div>
+                <div class="col-md-6">
+                    <textarea name="message" id="message" class="form-control" style="width: 400px; margin-left: -820px;"></textarea>
+                </div>
+            </div>
+            <div>
+                <input class="btn btn-success" type="submit" value="Apply" style="width: 30%; margin-left: -130px;">
+            </div>
+            </div>
+        </form>
+        
+    </div>
+    <footer style="background-color: black; height: 50px; width: 100%;">
+        <center>
+        <h1 style="color: white; top: 20%; position: relative; font-size: 20px;">All &copy;copyright reserved by G-tech webs limited</h1>
+        </center>
+    </footer>
+</body>
+</html>
