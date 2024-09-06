28d3b86b01a19e542d027c3f188bcbf509ddb61e
ishimweghislain <ishimweghislain82@gmail.com>
2024-09-05 22:50:23 -0700

    added dashboards

--- studenthome.php
+++ studenthome.php
@@ -15,9 +15,78 @@ if(!isset($_SESSION["username"])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1IbXyVY4AnDQlJlnGp1JcuT7jzwjju9SvrLVRyOoPp2hbWrGmJQ1iXwJSp" crossorigin="anonymous">
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-TcSIqQb027qvyjSMfHjOMaLkfuwVxZzUPnCJA7lzmcCWNIP3zVgnCgpD7Xa" crossorigin="anonymous"></script>
+    <style> 
+    *{
+        margin: 0;
+        padding: 0;
+    }
+        header{
+            background-color: skyblue;
+            line-height: 70px;
+            padding-left: 30px;
+        }
+        a,a:hover{
+            text-decoration: none!important;
+        }
+       .logout{
+        float: right;
+        padding-right: 30px;
+
+       }
+     ul{
+        background-color: #424a5b;
+        width: 16%;
+        height: 100%;
+        position: fixed;
+        padding-top: 5%;
+        text-align: center;
+     }
+
+     ul li{
+        list-style: none;
+        padding-bottom: 30px;
+        font-size: 15px;
+     }
+     ul li a{
+        color: white;
+        font-weight : bold;
+     }
+     ul li a:hover{
+        text-decoration: none;
+        color: skyblue;
+     }
+     .content{
+        margin-left: 20%;
+        margin-top: 5%;
+     }
+    </style>
 </head>
 <body>
-    <h1>hi student</h1>
-    <a href="logout.php">Logout</a>
+ <header>
+   <a href="">Student  Dashboard</a>
+   <div class="logout">
+    <a href="login.php" class="btn btn-primary">Logout</a>
+   </div>
+</header>
+<aside>
+    <ul>
+        <li><a href="">My courses</a></li>
+        <li><a href="">My Result</a></li>
+     
+    </ul>
+</aside>
+<div class="content">
+    <h1>Sidebar Accordion</h1>
+    <p>In this system bla bla bla continue reading stupid words ask the maker
+        lalal lalal lalal hello how are you
+    </p>
+    <input type="text" name="" id="">
+</div>
+
 </body>
 </html>
